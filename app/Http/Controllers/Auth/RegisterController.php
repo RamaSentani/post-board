5be7ff7aabf3c5fcc854cad14b02c3211795742f
Rama Sentani <sentanirama@gmail.com>
2021-09-13 18:55:47 +0700

    add login features

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -14,7 +14,6 @@ class RegisterController extends Controller
     }
 
     public function store(Request $request){
-        // Validation
         $this->validate($request, [
             'name' => 'required|max:255',
             'username' => 'required|max:255',
@@ -28,6 +27,8 @@ class RegisterController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        auth()->attempt($request->only('email', 'password'));
 
         return redirect()->route('dashboard');
     }
