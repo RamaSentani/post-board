de9f2bf2fe8f9b5692f23c36e3c8d1acc6aa6961
Rama Sentani <sentanirama@gmail.com>
2021-09-14 19:31:01 +0700

    list post and pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,11 @@ class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::paginate(20);
+
+        return view('posts.index', [
+            'posts' => $posts
+        ]);
     }
 
     public function store(Request $request)
